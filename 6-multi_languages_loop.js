f068f2efaed62e1fafd317e11b1867c2e0afc6ac
Isaac Lemayian <105207068+Lemayian-dev@users.noreply.github.com>
2025-11-14 09:29:46 +0000

    Fixed assignment 6

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -7,8 +7,7 @@ const lines = [
 let output = "";
 
 for (let i = 0; i < lines.length; i++) {
-  output += lines[i];
-  if (i < lines.length - 1) output += "\n";  // this is NOT an if/else statement block
+  output += lines[i] + "\n";
 }
 
-console.log(output);
+console.log(output.trim());
